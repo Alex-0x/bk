fca2ba6ac4aa972f41b1ceab36d0d0875361faac
Alessandro <101320750+Alex-0x@users.noreply.github.com>
2024-12-17 19:06:02 +0100

    .

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.get("/users", async (req, res) => {
 		if (result.rows.length === 0) {
 			return res.status(404).json({ error: "users not found" });
 		}
-		res.json(result.rows[0]);
+		res.json(result.rows);
 	} catch (err) {
 		res.status(500).json({ error: err.message });
 	}
